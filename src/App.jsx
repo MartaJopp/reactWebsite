e1b59ebd0761261dd4648f81f9665c297c5efe95
Marta Jopp <martapeterson@gmail.com>
2018-05-24 23:39:57 -0500

    imports to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import News from './components/News';
 
 class App extends Component {
   render() {
