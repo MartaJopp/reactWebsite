b5aebf66059e725846e00fd308b5047dc88db98e
Marta Jopp <martapeterson@gmail.com>
2018-05-25 20:41:12 -0500

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,10 +11,27 @@ export default class Home extends Component {
                 <Jumbotron>
                 <h2> Welcome Welcome </h2>
                 <p> Website </p>
+                    <Link to="/about">
+                        <Button bsStyle="primary">About </Button>
+                    </Link>
                 </Jumbotron>
-            <Link to="/about">
-<Button bsStyle="primary">About </Button> 
-            </Link>
+<Row className="show-grid text-center">
+<Col xs={12} sm={4} className="person-wrapper">
+<Image src="assets/1 month.JPG" circle className="profile-pic"/>
+            <h3> 1 Month </h3>
+            <p>Text text text </p>
+            </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/3 months.JPG" circle className="profile-pic" />
+                        <h3> 3 Months </h3>
+                        <p>Text text text </p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/2 months 2.JPG" circle className="profile-pic" />
+                        <h3> 2 Months </h3>
+                        <p>Text text text </p>
+                    </Col>
+            </Row>
             </Grid>
         )
     }
